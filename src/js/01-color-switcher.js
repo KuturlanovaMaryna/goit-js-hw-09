b79135a59e12e86ea48148bcc1d3245e8ebbc6fb
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-09-01 19:36:33 +0300

    continue task 1 && start task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+import '../css/common.css';
 
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+// let intervalId = null;
+
+startBtnEl.addEventListener('click', () => {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+  intervalId = setInterval(() => {
+   document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtnEl.addEventListener('click', () => {
+  stopBtnEl.disabled = true;
+  startBtnEl.disabled = false;
+  clearInterval(intervalId);
+});
