a78fca91d71c8b8611cd8db17cd8fce411cba2aa
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-09-02 18:11:16 +0300

    Update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,7 +8,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-// let intervalId = null;
+let intervalId = null;
 
 startBtnEl.addEventListener('click', () => {
   startBtnEl.disabled = true;
